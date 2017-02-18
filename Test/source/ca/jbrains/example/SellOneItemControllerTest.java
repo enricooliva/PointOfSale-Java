b999a08199386424dc6d678fc88aa0f9cdd6c606
enricooliva <oliva.enrico@gmail.com>
2017-02-18 18:43:19 +0100

    add sell controller test with irrilevant price

---
+++ Test/source/ca/jbrains/example/SellOneItemControllerTest.java
@@ -0,0 +1,68 @@
+package ca.jbrains.example;
+
+import org.jmock.Expectations;
+import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * Created by enricooliva on 18/02/2017.
+ */
+public class SellOneItemControllerTest {
+
+    @Rule
+    public final JUnitRuleMockery context = new JUnitRuleMockery();
+
+    @Test
+    public void productFound() throws Exception {
+        final Catalog catalog = context.mock(Catalog.class);
+        final Display display = context.mock(Display.class);
+        final Price irrilevantPrice = Price.cents(11234);
+
+        context.checking(new Expectations(){{
+            allowing(catalog).findPrice(with("12345"));
+            will(returnValue(irrilevantPrice));
+
+            oneOf(display).displayPrice(with(irrilevantPrice));
+        }});
+
+        SaleController saleController = new SaleController(display, catalog);
+        saleController.onBarcode("12345");
+
+        //context.assertIsSatisfied();
+    }
+
+    public interface Catalog{
+        Price findPrice (String barcode);
+    }
+
+    public interface Display{
+        void displayPrice(Price price);
+    }
+
+    public static class Price {
+        public static Price cents(int centsValue){
+            return new Price();
+        }
+
+        @Override
+        public String toString() {
+            return "a Price";
+        }
+    }
+
+    public static class SaleController{
+        private Display display;
+        private Catalog catalog;
+
+        public SaleController(Display display, Catalog catalog){
+            this.display = display;
+            this.catalog = catalog;
+        }
+
+        public void onBarcode(String barcode){
+            display.displayPrice(catalog.findPrice(barcode));
+        }
+    }
+
+}
